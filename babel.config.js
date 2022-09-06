9387a316f4bedd0488f548b21d2469e4a13589e8
fabio oliveira costa <fabio@heydaypeople.com>
2022-09-06 08:14:27 +0200

    chore: turned ts config into alias within babel

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,22 @@
  *
  * @todo Figure out how to make it simpler, if we could only use the Next.JS , storybook tooling that would be better
  */
+
+const {
+  compilerOptions: { paths },
+} = require("./tsconfig.json");
+
+function turnTsConfigPathIntoAlias() {
+  return Object.entries(paths).reduce(
+    (acc, [key, [value]]) => ({
+      ...acc,
+      [key.replace("/*", "")]: `./${value.replace("/*", "")}`,
+    }),
+    {}
+  );
+}
+const alias = turnTsConfigPathIntoAlias();
+
 module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
@@ -13,10 +29,7 @@ module.exports = {
     [
       "module-resolver",
       {
-        alias: {
-          "@components": "./ui-components",
-          "@stories": "./stories",
-        },
+        alias,
       },
     ],
   ],
